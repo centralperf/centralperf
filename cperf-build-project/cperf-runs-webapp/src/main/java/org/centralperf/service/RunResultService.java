485fb6d349fd8306ce39d48bec893a45b8ba4dc7
Charles Le Gallic <charles@amoae.com>
2013-10-10 00:30:33 +0200

    Import JTL files feature

--- cperf-build-project/cperf-runs-webapp/src/main/java/org/centralperf/service/RunResultService.java
+++ cperf-build-project/cperf-runs-webapp/src/main/java/org/centralperf/service/RunResultService.java
@@ -3,8 +3,11 @@ package org.centralperf.service;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -38,35 +41,66 @@ public class RunResultService {
 	 */
 	public void saveResults(Run run){
 		String resultInCSV = run.getRunResultCSV();
-		CSVReader csvReader = new CSVReader(new StringReader(resultInCSV));
-
-		String[] nextLine = null;
-		try {
-			while ((nextLine = csvReader.readNext()) != null) {
-			    int size = nextLine.length;
-			    log.debug(" SAVE RESULT : SIZE["+size+"]");
-			    // empty line
-			    if (size == 0) {
-			        continue;
-			    }
-			    
-			    Sample sample = new Sample();
-			    sample.setRun(run);
-			    sample.setTimestamp(new Date(new Long(nextLine[0].trim())));
-			    sample.setElapsed(new Long(nextLine[1].trim()));
-			    sample.setSampleName(nextLine[2].trim());
-			    sample.setStatus(new Integer(nextLine[3].trim()));
-			    sample.setAssertResult(new Boolean(nextLine[7].trim()));
-			    sample.setSizeInOctet(new Long(nextLine[8].trim()));
-			    sample.setLatency(new Long(nextLine[9].trim()));
-			    sampleRepository.save(sample);
-			    log.debug("Adding Sample :"+sample);
-			}
-		} catch (IOException e) {
-			// Impossible to access to the file ?!
-		}
+        saveResults(run, resultInCSV);
 	}
-	
+
+    public void saveResults(Run run, String resultInCSV){
+        CSVReader csvReader = new CSVReader(new StringReader(resultInCSV));
+        String[] nextLine = null;
+        CSVHeaderInfo headerInfo = new CSVHeaderInfo("timeStamp,elapsed,label,responseCode,responseMessage,threadName,dataType,assertionResult,bytes,Latency".split(","));
+        try {
+            while ((nextLine = csvReader.readNext()) != null) {
+                int size = nextLine.length;
+
+                // empty line
+                if (size == 0) {
+                    continue;
+                }
+                // Header line (for uploaded files)
+                if("timestamp".equals(nextLine[0].trim().toLowerCase())){
+                    headerInfo = new CSVHeaderInfo(nextLine);
+                    continue;
+                }
+
+                Sample sample = new Sample();
+                sample.setRun(run);
+                try{
+                    // Try first to get a timestamp
+                    sample.setTimestamp(new Date(new Long(headerInfo.getValue("timestamp",nextLine))));
+                }
+                catch(NumberFormatException e){
+                    // Try to parse this format : 2012/10/30 12:47:47
+                    SimpleDateFormat parserSDF=new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                    try {
+                        sample.setTimestamp(parserSDF.parse(headerInfo.getValue("timestamp",nextLine)));
+                    } catch (ParseException e1) {
+                        // TODO : return parsing error
+                    }
+                }
+
+                sample.setElapsed(new Long(headerInfo.getValue("elapsed",nextLine)));
+                sample.setSampleName(headerInfo.getValue("label",nextLine));
+                try{
+                    sample.setStatus(new Integer(headerInfo.getValue("responseCode",nextLine)));
+                }
+                catch (NumberFormatException e){
+                    sample.setStatus(-1);
+                }
+                sample.setAssertResult(new Boolean(headerInfo.getValue("assertionResult",nextLine)));
+                sample.setSizeInOctet(new Long(headerInfo.getValue("bytes",nextLine)));
+                sample.setLatency(new Long(headerInfo.getValue("latency",nextLine)));
+                sampleRepository.save(sample);
+                log.debug("Adding Sample :"+sample.getSampleName());
+            }
+        } catch (IOException e) {
+            // Impossible to access to the file ?!
+        }
+    }
+
+    private String getColumnValue(String[] CSVHeaders, String[] CSVLine, String columnName){
+        return CSVLine[java.util.Arrays.asList(CSVHeaders).indexOf(columnName)].trim();
+    }
+
 	public RunResultSummary getSummaryFromRun(Run run){
 		if(run.isLaunched()){
 			return getSummaryFromCSVString(run.getRunResultCSV());
@@ -92,9 +126,32 @@ public class RunResultService {
 		summary.setNumberOfSample(lines.size());
 		if (lines.size() > 0) {
 			String lastLine = lines.get(lines.size() - 1);
-			long lastLineDateAsLong = new Long(lastLine.split(",")[0]);
-			summary.setLastSampleDate(new Date(lastLineDateAsLong));
+            try{
+			    long lastLineDateAsLong = new Long(lastLine.split(",")[0]);
+			    summary.setLastSampleDate(new Date(lastLineDateAsLong));
+            }
+            catch(NumberFormatException e){
+                // TODO : Handle various CSV formats
+            }
 		}
 		return summary;
 	}
+
+    private class CSVHeaderInfo{
+
+        private String[] headers;
+        private HashMap<String, Integer> headersIndex = new HashMap<String, Integer>();
+
+        public CSVHeaderInfo(String[] headers){
+            this.headers = headers;
+            for (int i=0; i<headers.length; i++){
+                headersIndex.put(headers[i].trim().toLowerCase(), i);
+            }
+        }
+
+        public String getValue(String headerName, String[] CSVline){
+            return CSVline[headersIndex.get(headerName.toLowerCase())];
+        }
+
+    }
 }
