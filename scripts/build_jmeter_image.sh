5637e4555dd9403f658cd90647f8852800a32523
Charles Le gallic <charles@amoae.com>
2020-04-09 11:57:23 +0200

    Allow to pass JVM_ARGS for jMeter Dockerfile

--- scripts/build_jmeter_image.sh
+++ scripts/build_jmeter_image.sh
@@ -7,4 +7,10 @@ cd ${DIR}/../src/main/docker/jmeter/
 # Load jMeter version from Docker Compose .env
 source ${DIR}/../src/main/docker/.env
 echo "Build jMeter Docker Image with version ${JMETER_VERSION}"
-docker build -t centralperf_jmeter:latest --build-arg JMETER_VERSION=${JMETER_VERSION} .
+
+# Allows to set additional JVM_ARGS, especially for proxy settings if any. For example : ./build_jmeter_image.sh "-Dhttps.proxyHost=proxy.local -Dhttps.proxyPort=3128"
+JVM_ARGS=""
+if [[ -n "$1" ]]; then
+  JVM_ARGS="$1"
+fi
+docker build -t centralperf_jmeter:latest --build-arg JMETER_VERSION="${JMETER_VERSION}" --build-arg JVM_ARGS="${JVM_ARGS}" .
