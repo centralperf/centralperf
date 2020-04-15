fb18510f20b2b8330e2378ca74c8b109dd21e077
Charles Le gallic <charles@amoae.com>
2020-04-15 16:47:40 +0200

    Disable CSRF filter

--- src/main/java/org/centralperf/config/SecurityConfig.java
+++ src/main/java/org/centralperf/config/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+                .and().csrf().disable();
     }
 }
